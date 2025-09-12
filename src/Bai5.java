8d316e0aab88859f55e05d00540c9bd8dff4f16b
CGM <mrmouse.guru@gmail.com>
2025-09-12 11:28:17 +0700

    VẤN ĐỀ NHẬP - Scanner

---
+++ src/Bai5.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Bai5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner keyboard;
+		String name;
+		
+		System.out.print("[HÃY NHẬP VÀO TÊN ĐI BẠN]:");
+		keyboard = new Scanner(System.in);
+		
+		name = keyboard.nextLine();
+		
+		
+		
+		System.out.println("Hello: " + name);
+
+	}
+
+}
