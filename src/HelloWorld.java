628f932eb105d9ece2f6165d46fb955c4eec9d30
CGM <mrmouse.guru@gmail.com>
2025-09-12 10:49:34 +0700

    LIÊN QUAN ĐẾN OUTPUT - print/ln/f

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,6 +4,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Hello Wolrd");
+		System.out.println("Hello World");
+		System.out.printf("%s %d", "Hello World", 6);
 	}
 
 }
